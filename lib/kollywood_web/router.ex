7c33c1ee379f9f79102e38b2745b9af53206d8c3
Manoj Mahalingam <manojlds@gmail.com>
2026-03-21 09:56:05 +0000

    Initial Phoenix scaffold for Kollywood - multi-agent orchestrator

---
+++ lib/kollywood_web/router.ex
@@ -0,0 +1,27 @@
+defmodule KollywoodWeb.Router do
+  use KollywoodWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {KollywoodWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", KollywoodWeb do
+    pipe_through :browser
+
+    get "/", PageController, :home
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", KollywoodWeb do
+  #   pipe_through :api
+  # end
+end
